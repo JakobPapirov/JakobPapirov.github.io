414d85c6be2799ac1021a0f214e05492b85a6b40
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2020-06-28 11:16:12 +0200

    First redesign footer.js commit

---
+++ js/library/footer.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+	$(".footer-left").html(`
+		<img src="graphics/GitHub-32.png" alt="Github icon" title="">`);
+
+	$(".footer-middle").html(`
+		<img src="graphics/icons8-instagram-32.png" alt="Instagram icon" title="">
+		<img src="graphics/icons8-twitter-32.png" alt="Twitter icon" title="">
+		<img src="graphics/icons8-play-button-32.png" alt="Youtube icon" title="">`);
+
+	$(".footer-right").html(`
+		<img src="graphics/icons8-linkedin-32.png" alt="LinkedIn icon" title=""> 
+		<img src="graphics/icons8-gmail-32.png" alt="Gmail icon">
+		<img src="graphics/icons8-skype-32.png" alt=""Skype icon>`);
+});
